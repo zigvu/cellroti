01d8b253403f2675ab9c21791d8afc8f371a43e9
eacharya <eacharya@gmail.com>
2015-01-07 10:05:55 -0800

    Issue 2: Dynamic active class in navigation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,4 +23,5 @@
 
 $(function(){ 
 	$(document).foundation(); 
+	$(document).ready(decorateNavigationList);
 });
--- app/assets/javascripts/helper_functions.js
+++ app/assets/javascripts/helper_functions.js
@@ -3,6 +3,20 @@
 	------------------------------------------------*/
 
 //------------------------------------------------
+/* Decorate top-bar navigation li items with "active" class */
+function decorateNavigationList(){
+	// for each page, we set class to controller name
+	var bodyClass = $('body').attr('class').split(" ");
+	for (var i = 0, l = bodyClass.length; i < l; ++i) {
+		// get the right element - assume id is set for each li
+		var navElem = $("li#" + bodyClass[i]);
+		// set active class
+		$("nav.top-bar").find(navElem).attr('class', 'active');
+	}
+}
+//------------------------------------------------
+
+//------------------------------------------------
 /* Sort objects with IDs */
 function sortById(a, b){
   var aId = a.id;
